399cf40b1c2d1ee351fa1e97096bf7fa58a90412
Cristina Izquierdo <62721902+CristinaIzquierdo2020@users.noreply.github.com>
2021-03-16 15:00:20 +0100

    Delete psp directory

--- psp/src/main/java/chat/MaxConnections.java
+++
@@ -1,9 +0,0 @@
-package chat;
-
-public class MaxConnections extends Exception {
-	private static final long serialVersionUID = -1002766384869121579L;
-
-	public String getMessage() {
-		return "Se ha alcanzado el numero maximo de conexiones";
-	}
-}
